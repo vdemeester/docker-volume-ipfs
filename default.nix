ed0db7a0497ada063d7ccb4b92a231927e4918d7
Vincent Demeester <vincent@sbr.pm>
2017-01-16 22:50:59 +0100

    Add .envrc and default.nix
    If you have direnv and nix package system, you're gonna like that,
    easy setup :)
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ default.nix
@@ -0,0 +1,19 @@
+let
+  _pkgs = import <nixpkgs> {};
+in
+{ pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
+                                         repo = "nixpkgs-channels";
+                                         rev = "fd3d2b1a8dca6a4a90093a54cecbdb3e66f163fb";
+                                         sha256 = "06cj963b59l29vzsk4d0g89v4kjg4ycgvbgz135yyqfrnn5hlhzn";
+                                       }) {}
+}:
+# FIXME(vdemeester): pin nixpkgs version
+pkgs.stdenv.mkDerivation rec {
+    name = "docker-volume-ipfs-dev";
+    env = pkgs.buildEnv { name = name; paths = buildInputs; };
+    buildInputs = [
+        pkgs.vndr
+        pkgs.go_1_7
+        pkgs.gnumake
+    ];
+}
